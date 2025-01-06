867f829e720fbc666d993dfcc250323a570ea1d7
VICTOR <olamuyiwavictor55@gmail.com>
2025-01-06 23:14:49 +0100

    responsiveness

--- src/components/AI.jsx
+++ src/components/AI.jsx
@@ -236,8 +236,9 @@ const Desc = styled.p`
   @media only screen and (max-width: 420px) {
     font-size: 0.8rem;
     padding: 0 1rem;
-    margin-top:-1px;
+    margin-top:-200px;
     color:white;
+    z-index:10;
   }
   @media only screen and (max-width: 768px) and (min-width: 422px) {
     font-size: 0.8rem;
