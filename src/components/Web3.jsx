644f49badff95a318385a3b426f190deb86fcd6b
VICTOR <olamuyiwavictor55@gmail.com>
2025-01-06 23:09:58 +0100

    responsiveness

--- src/components/Web3.jsx
+++ src/components/Web3.jsx
@@ -37,7 +37,151 @@ const projects = [
  
 ];
 
-portfolio-main/src/components/Store.jsx
+const CarouselContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 20rem;
+  width: 33rem;
+  position: absolute;
+  background-color: inherit;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  overflow: hidden;
+
+  // Mobile-first responsive design
+  @media (max-width: 480px) {
+    height: 15rem;
+    width: 21rem;
+    top: -50px;
+  }
+
+  @media (min-width: 481px) and (max-width: 768px) {
+    height: 22rem;
+    width: 31rem;
+    top: -50px;
+  }
+
+  @media (min-width: 769px) and (max-width: 1024px) {
+    height: 17rem;
+    width: 30rem;
+    top: -800px;
+  }
+
+  @media (min-width: 1025px) {
+    height: 20rem;
+    width: 33rem;
+    top: 0; // Reset to center for larger screens
+  }
+`;
+
+const CarouselWrapper = styled.div`
+  display: flex;
+  height: 100%;
+  transform: translateX(-${({ activeIndex }) => activeIndex * 100}%);
+  transition: transform 0.5s ease;
+`;
+
+const CarouselCard = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  flex-shrink: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #f8f8f8;
+  border-radius: 20px;
+  margin: auto;
+`;
+
+const CardImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  border-radius: 1.25rem;
+`;
+
+const CardTitle = styled.h3`
+  font-size: 1.4rem;
+  color: #da4ea2;
+  cursor: pointer;
+  @media only screen and (max-width: 420px) {
+    font-size: 1rem;
+  }
+
+  @media only screen and (max-width: 768px) and (min-width: 422px) {
+    font-size: 1.1rem;
+  }
+  @media only screen and (max-width: 1024px) and (min-width: 769px) {
+    font-size: 1.2rem;
+  }
+`;
+
+const NextButton = styled.button`
+  position: absolute;
+  right: 10px;
+  padding: 0;
+  border: none;
+  cursor: pointer;
+  background-color: inherit;
+  z-index: 1;
+`;
+
+const PrevButton = styled.button`
+  position: absolute;
+  left: 10px;
+  border: none;
+  cursor: pointer;
+  background-color: inherit;
+  z-index: 1;
+  @media only screen and (max-width: 1024px) and (min-width: 769px) {
+    height: 15rem;
+    justify-content: flex-start;
+    z-index: 0;
+  }
+`;
+
+const ArrowImg = styled.img`
+  width: 2em;
+  height: 2em;
+  padding: 0;
+`;
+
+const ProjectLinks = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  @media only screen and (max-width: 768px) and (min-width: 422px) {
+    width: 70%;
+  }
+  @media only screen and (max-width: 1024px) and (min-width: 769px) {
+    width: 80%;
+  }
+`;
+
+const Link = styled.img`
+  width: 3rem;
+  height: 3rem;
+
+  @media only screen and (max-width: 420px) {
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+  @media only screen and (max-width: 768px) and (min-width: 422px) {
+    width: 1.7rem;
+    height: 1.7rem;
+  }
+
+  @media only screen and (max-width: 1024px) and (min-width: 769px) {
+    width: 2rem;
+    height: 2rem;
+  }
+`;
 const Details = styled.div`
   display: flex;
   flex-direction: column;
